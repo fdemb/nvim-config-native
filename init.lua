b0f9ca7a2a488a28e135f53c4b3d39c424f11b38
fdemb <69842744+fdemb@users.noreply.github.com>
2025-11-20 22:29:25 +0100

    catppuccin

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ vim.o.smartcase = true
 vim.o.ignorecase = true
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.opt.cmdheight = 0
 
 -- disable unneeded providers
 vim.g.loaded_node_provider = 0
@@ -23,40 +24,97 @@ vim.g.loaded_ruby_provider = 0
 
 -- Plugins
 vim.pack.add({
-    "https://github.com/rebelot/kanagawa.nvim",
+    -- theme
+    "https://github.com/catppuccin/nvim",
+
+    -- completion
+    "https://github.com/rafamadriz/friendly-snippets",
+    { src = "https://github.com/saghen/blink.cmp", version = "v1.8.0" },
+
+    -- mini
     "https://github.com/echasnovski/mini.statusline",
     "https://github.com/echasnovski/mini.pick",
-    "https://github.com/echasnovski/mini.completion",
     "https://github.com/echasnovski/mini.icons",
     "https://github.com/echasnovski/mini-git",
+
+    -- file "tree"
     "https://github.com/stevearc/oil.nvim",
+
+    -- formatting
+    "https://github.com/stevearc/conform.nvim",
+
+    -- syntax highlighting
     "https://github.com/nvim-treesitter/nvim-treesitter",
+
+    -- lsp
     "https://github.com/neovim/nvim-lspconfig",
+
+    -- ai completion
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/milanglacier/minuet-ai.nvim"
 })
 
 -- Plugin setup
 require("mini.icons").setup()
 require("mini.statusline").setup()
 require("mini.pick").setup()
-require("mini.completion").setup()
 require("mini.git").setup()
 require("oil").setup()
+
+require("conform").setup({
+    formatters_by_ft = {
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+    }
+})
+
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "vue", "typescript", "javascript", "lua" },
     highlight = { enable = true },
 })
-require("kanagawa").setup({
-    colors = {
-        theme = {
-            all = {
-                ui = {
-                    bg_gutter = "none"
-                }
-            }
-        }
-    }
+vim.cmd.colorscheme("catppuccin-mocha")
+
+require('minuet').setup({
+    provider = "gemini",
 })
-vim.cmd.colorscheme("kanagawa-wave")
+
+require('blink-cmp').setup {
+    keymap = {
+        -- Manually invoke minuet completion.
+        ['<C-a>'] = require('minuet').make_blink_map(),
+    },
+    sources = {
+        -- Enable minuet for autocomplete
+        default = { 'lsp', 'path', 'buffer', 'snippets', 'minuet' },
+        providers = {
+            minuet = {
+                name = 'minuet',
+                module = 'minuet.blink',
+                async = true,
+                -- Should match minuet.config.request_timeout * 1000,
+                -- since minuet.config.request_timeout is in seconds
+                timeout_ms = 3000,
+                score_offset = 50, -- Gives minuet higher priority among suggestions
+            },
+        },
+    },
+    -- Recommended to avoid unnecessary request
+    completion = { trigger = { prefetch_on_insert = false } },
+}
+
+-- Custom commands and autocmds
+vim.api.nvim_create_user_command("Format", function(args)
+    local range = nil
+    if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+            start = { args.line1, 0 },
+            ["end"] = { args.line2, end_line:len() },
+        }
+    end
+    require("conform").format({ async = true, lsp_format = "fallback", range = range })
+end, { range = true })
 
 -- Keybindings
 vim.keymap.set("n", "<Leader>h", MiniPick.builtin.help)
@@ -66,8 +124,8 @@ vim.keymap.set("n", "<Leader>g", MiniPick.builtin.grep_live)
 vim.keymap.set("n", "<Leader>ec", ":e ~/.config/nvim/init.lua<CR>")
 vim.keymap.set("n", "<Leader>w", ":w<CR>")
 vim.keymap.set("n", "-", ":Oil<CR>")
-vim.keymap.set("n", "<Leader>fm", vim.lsp.buf.format)
-vim.keymap.set({"n", "i"}, "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "<Leader>fm", ":Format<CR>")
+vim.keymap.set({ "n", "i" }, "gd", vim.lsp.buf.definition)
 
 -- LSP
 vim.lsp.enable("lua_ls")
@@ -76,6 +134,7 @@ vim.lsp.enable('vue_ls')
 vim.lsp.enable('vtsls')
 vim.lsp.enable('ts_ls')
 vim.lsp.enable('astro')
+vim.lsp.enable('tailwindcss')
 
 local vue_language_server_path = '/opt/homebrew/lib/node_modules/@vue/language-server'
 local vue_plugin = {
@@ -123,5 +182,3 @@ if vim.g.neovide then
     vim.g.neovide_cursor_short_animation_length = 0
     vim.g.neovide_cursor_trail_size = 0
 end
-
-
